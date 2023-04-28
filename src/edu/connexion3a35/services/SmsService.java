95de4faee1d5ac01930a07b049ce937c2f0ab8f8
blast <fberrzig@gmail.com>
2023-04-28 14:46:19 +0100

    background and design

--- src/edu/connexion3a35/services/SmsService.java
+++ src/edu/connexion3a35/services/SmsService.java
@@ -7,15 +7,15 @@ import com.twilio.type.PhoneNumber;
 public class SmsService {
     // Find your Account SID and Auth Token at twilio.com/console
     // and set the environment variables. See http://twil.io/secure
-    public static final String ACCOUNT_SID = "ACdf64d915fe8a1720a915295b78d3907c";
-    public static final String AUTH_TOKEN = "ee94861c253f31351905139994d79dfe";
-
+    public static final String ACCOUNT_SID = "ACa115369491768f0709b6b633c93e1e3d";
+    public static final String AUTH_TOKEN = "1e19cba7ae58fc1a0bd99b292f1c52c0";
+    public static final String TWILIO_NUMBER = "+16205079772";
 
     public void sendSms(String phone,String code,String name) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                         new com.twilio.type.PhoneNumber("+216"+phone),
-                        new com.twilio.type.PhoneNumber("+16318835776"),
+                        new com.twilio.type.PhoneNumber(TWILIO_NUMBER),
                         "Dear "+name+"\n\nPlease confirm your account by entering the following code: "+code+"\n\n AidMe Bot")
                 .create();
 
@@ -25,7 +25,7 @@ public class SmsService {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                         new com.twilio.type.PhoneNumber("+216"+phone),
-                        new com.twilio.type.PhoneNumber("+16318835776"),
+                        new com.twilio.type.PhoneNumber(TWILIO_NUMBER),
                         "Dear "+name.toUpperCase()+ " \n\nYour reset password code is :  "+code+"\n\n AidMe Bot")
                 .create();
 
@@ -35,7 +35,7 @@ public class SmsService {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
                         new com.twilio.type.PhoneNumber("+216"+phone),
-                        new com.twilio.type.PhoneNumber("+16318835776"),
+                        new com.twilio.type.PhoneNumber(TWILIO_NUMBER),
                         "Dear "+name.toUpperCase()+ " \n\nYour account has been banned for violating our terms of use.\n\n AidMe Bot")
                 .create();
 
